0036e44dc940ec663351a81b19f1a07bed9f64cf
Sigurd Schoeler <sigurd@finexa.no>
2024-07-11 11:59:46 +0200

    More changes to create better compare output

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,11 @@ pnpm run serve
 
 # Find all files recursively except node_modules and sort them
 echo "Listing all files except node_modules:"
-find . -type d -name node_modules -prune -o -type f -print | sort
+find . -type d \( -name node_modules -o -name .git \) -prune -o -type f -print | sort
 
 # Echo a visual splitter
 echo "========================"
 
 # List node modules with pnpm (without parseable option)
 echo "Listing node modules:"
-pnpm list
+pnpm list --parseable
